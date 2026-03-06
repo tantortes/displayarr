013ac19733880fc4fbf41d0bb655453546abd901
tantortes <tantortes@gmail.com>
2026-03-06 01:23:55 -0500

    Update sonarr_client.php

--- include/sonarr_client.php
+++ include/sonarr_client.php
@@ -1,10 +1,24 @@
-  function sonarr_request($endpoint) {
-      $url = $GLOBALS['sonarr_url'] . "/api/v3/" . $endpoint;
-      $opts = ["http" => ["method" => "GET", "header" => "X-Api-Key: " .
-  $GLOBALS['sonarr_api_key']]];
-      $context = stream_context_create($opts);
-      $result = file_get_contents($url, false, $context);
-           5
+  <?php
 
-       return json_decode($result, true);
-  }
+function sonarr_request($endpoint, $url, $apikey) {
+
+    $full = rtrim($url,'/') . "/api/v3/" . $endpoint;
+
+    $opts = [
+        "http" => [
+            "method" => "GET",
+            "header" => "X-Api-Key: $apikey\r\n",
+            "timeout" => 5
+        ]
+    ];
+
+    $context = stream_context_create($opts);
+
+    $result = @file_get_contents($full, false, $context);
+
+    if ($result === false) {
+        return null;
+    }
+
+    return json_decode($result, true);
+}
